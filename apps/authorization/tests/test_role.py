d355ae2e9e79e9c7adfcd321a5fe7fab3f620ab8
Jhon Wilmer Azcarate Rivera <jhonazcaratework@gmail.com>
2025-05-11 20:15:46 -0500

    feature - can update role

--- apps/authorization/tests/test_role.py
+++ apps/authorization/tests/test_role.py
@@ -4,7 +4,7 @@ from rest_framework.test import APITestCase
 from rest_framework_simplejwt.tokens import AccessToken
 from django.apps import apps
 
-class RolCreateTests(APITestCase):
+class RoleCreateTests(APITestCase):
     
     def setUp(self):
         self.url = reverse('role-list')
@@ -24,7 +24,7 @@ class RolCreateTests(APITestCase):
         self.access_token = AccessToken.for_user(self.user)
         self.client.credentials(HTTP_AUTHORIZATION='Bearer ' + str(self.access_token))
         
-    def test_rol_create_with_valid_data(self):
+    def test_role_create_with_valid_data(self):
         data = {**self.data}
         response = self.client.post(self.url, data,format='json')
         self.assertEqual(response.status_code,status.HTTP_201_CREATED,'The role was not created')
@@ -32,7 +32,7 @@ class RolCreateTests(APITestCase):
         expected_name = self.data['name'].lower().replace(' ', '_')
         self.assertEqual(response.data.get('name'),expected_name,'The role name was not returned correctly')
     
-    def test_rol_create_with_invalid_name(self):
+    def test_role_create_with_invalid_name(self):
         data = {**self.data}
         
         self.client.post(self.url,self.data,format='json')
@@ -65,4 +65,55 @@ class RolCreateTests(APITestCase):
         self.assertEqual(response.status_code,status.HTTP_400_BAD_REQUEST,'The role was created with a name that contains more than 30 characters')
         self.assertIn('name',response.data,'Should return an error message for the name field')
         self.assertNotEqual(self.Role.objects.count(),3,'The role was registered with a name that contains more than 30 characters on the database')
+
+class RoleUpdateTests(APITestCase):
+    
+    def setUp(self):
+        self.Role = apps.get_model('authorization', 'Role')
+        self.role = self.Role.objects.create(name='test_role')
         
+        self.url = reverse('role-detail', kwargs={'pk': self.role.id})
+        
+        self.User = apps.get_model('authentication', 'User')
+        self.user = self.User.objects.create_user(
+            username='testuser',
+            email='testuser@example.com',
+            password='Password@1234567',
+            is_active=True
+        )
+        admin_role = self.Role.objects.filter(name='admin').first()
+        self.user.assign_roles([admin_role.id])
+        self.access_token = AccessToken.for_user(self.user)
+        self.client.credentials(HTTP_AUTHORIZATION='Bearer ' + str(self.access_token))
+    
+    def test_role_update_with_valid_data(self):
+        data = {'name': 'UPDATED_ROLE'}
+        response = self.client.put(self.url, data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_200_OK, 'Update failed with valid data')  # 200 OK expected
+        
+        self.role.refresh_from_db()
+        self.assertEqual(self.role.name, 'updated_role', 'Name not updated in database')  # Check normalization
+        
+    def test_role_update_with_invalid_name(self):
+        self.Role.objects.create(name='existing_role')
+        
+        data = {'name': 'existing_role'}
+        response = self.client.put(self.url, data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST, 'Allowed duplicate name in update')
+        self.assertIn('name', response.data, 'Error not linked to name field')
+        
+        data['name'] = 'invalid@name'
+        response = self.client.put(self.url, data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST, 'Allowed special characters')
+        
+        data['name'] = 'name123'
+        response = self.client.put(self.url, data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST, 'Allowed numbers in name')
+        
+        data['name'] = ''
+        response = self.client.put(self.url, data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST, 'Allowed empty name')
+        
+        data['name'] = 'a' * 31
+        response = self.client.put(self.url, data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST, 'Allowed over-length name')
